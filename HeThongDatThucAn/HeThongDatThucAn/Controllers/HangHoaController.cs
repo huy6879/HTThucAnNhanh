a3518471a9982447981887ecf95e7357f8e915c2
huy6879 <117825088+huy6879@users.noreply.github.com>
2024-05-12 01:02:54 +0700

    chia header /hanghoa

---
+++ HeThongDatThucAn/HeThongDatThucAn/Controllers/HangHoaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HeThongDatThucAn.Controllers
+{
+    public class HangHoaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
