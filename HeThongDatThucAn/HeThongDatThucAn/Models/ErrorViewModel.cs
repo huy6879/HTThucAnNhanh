a654e551a22877188f67f5b6e74ab420e8868019
buudinh16 <149130262+buudinh16@users.noreply.github.com>
2024-05-12 22:07:34 +0700

    Delete HeThongDatThucAn directory

--- HeThongDatThucAn/HeThongDatThucAn/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HeThongDatThucAn.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
