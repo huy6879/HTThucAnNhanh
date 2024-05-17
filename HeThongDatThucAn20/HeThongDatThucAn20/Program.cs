7ab6acdc0cf576878991eb1290e60d31038e6a21
huy6879 <117825088+huy6879@users.noreply.github.com>
2024-05-17 21:29:43 +0700

    qqqq

--- HeThongDatThucAn20/HeThongDatThucAn20/Program.cs
+++ HeThongDatThucAn20/HeThongDatThucAn20/Program.cs
@@ -10,6 +10,15 @@ builder.Services.AddDbContext<HeThongDatDoAnContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("HeThongDatDoAn"));
 });
 
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(10);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,6 +33,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseSession();
 
 app.UseAuthorization();
 
