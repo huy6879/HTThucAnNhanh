13a640df81a1fcf6ef323cab8cbd5a35fd6d50c4
buudinh16 <giangdinh2003@gmail.com>
2024-05-29 23:08:48 +0700

    tim kim chi nhanh

--- HeThongDatThucAn20/HeThongDatThucAn20/Controllers/BranchController.cs
+++ HeThongDatThucAn20/HeThongDatThucAn20/Controllers/BranchController.cs
@@ -1,6 +1,7 @@
 ﻿using HeThongDatThucAn20.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace HeThongDatThucAn20.Controllers
 {
@@ -28,6 +29,32 @@ namespace HeThongDatThucAn20.Controllers
             var details = db.Branches.FirstOrDefault(d => d.BranchId == id);
             ViewBag.branch = details;
             return View(details);
+        }
+        [HttpGet]
+        public ActionResult SearchByDistrict()
+        {
+            // Initially display all branches or an empty list
+            var branches = db.Branches.ToList();
+            return View(branches);
+        }
+
+        [HttpPost]
+        public ActionResult SearchByDistrict(string districtName)
+        {
+            if (districtName == null)
+            {
+                return RedirectToAction("ListBranch");
+            }
+            else
+            {
+                var filteredBranches = db.Branches
+                .Where(b => b.BranchDistrict.Contains(districtName))
+                .ToList();
+
+                ViewBag.DistrictName = districtName;
+                return View(filteredBranches);
+            }
+            
         }
     }
 }
