872522fc1417c53d1982a6f75e439b3dbdb5a843
huy6879 <117825088+huy6879@users.noreply.github.com>
2024-05-30 22:12:14 +0700

    cap nhat

--- HeThongDatThucAn20/HeThongDatThucAn20/Areas/Admin/Controllers/ProductsController.cs
+++ HeThongDatThucAn20/HeThongDatThucAn20/Areas/Admin/Controllers/ProductsController.cs
@@ -148,6 +148,43 @@ namespace HeThongDatThucAn20.Areas.Admin.Controllers
             ViewBag.CateId = new SelectList(cate, "CateId", "CategoryName");
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> Edit(ProductModels c, int id)
+        {
+            client.BaseAddress = new Uri(domain);
+
+            var formdata = new MultipartFormDataContent();
+
+            // Thêm các trường dữ liệu khác vào form data
+            formdata.Add(new StringContent(c.ProductId.ToString()), "ProductId");
+            formdata.Add(new StringContent(c.CateId.ToString()), "CateId");
+            formdata.Add(new StringContent(c.ProductName), "ProductName");
+            formdata.Add(new StringContent(c.UnitPrice.ToString()), "UnitPrice");
+            formdata.Add(new StringContent(c.Description), "Description");
+            formdata.Add(new StringContent(c.Status.ToString()), "Status");
+            formdata.Add(new StringContent(c.Quantity.ToString()), "Quantity");
+            formdata.Add(new StringContent(c.UnitInStock.ToString()), "UnitInStock");
+
+
+
+
+            // Gửi yêu cầu POST đến API với dữ liệu sản phẩm
+            var result = await client.PutAsync("/api/Product/update-product/" + id, formdata);
+
+            // Kiểm tra kết quả và xử lý
+            if (result.IsSuccessStatusCode)
+            {
+                // Nếu thành công, chuyển hướng đến trang ProductList
+                return RedirectToAction("ProductList");
+            }
+            else
+            {
+                // Xử lý lỗi nếu cần thiết
+                // Ví dụ: hiển thị thông báo lỗi
+                ModelState.AddModelError(string.Empty, "Có lỗi xảy ra khi tạo sản phẩm.");
+                return View(c); // Hoặc redirect đến một trang khác tùy theo logic của ứng dụng
+            }
+        }
 
     }
 }
